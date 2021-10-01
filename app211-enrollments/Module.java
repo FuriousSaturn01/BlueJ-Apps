76e64f5daae78b09e72c1b181510e143d6a5fe19
FuriousSaturn01 <91369232+FuriousSaturn01@users.noreply.github.com>
2021-10-01 12:44:23 +0100

    Create Module.java

---
+++ app211-enrollments/Module.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class Module here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Module
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Module
+     */
+    public Module()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+    
+}
